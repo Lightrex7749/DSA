26b218576fce033d7d05f171e60e1307eeae34e0
Lightrex <gauravrajbhar051@gmail.com>
2025-10-10 11:53:23 +0530

    yess upadated

--- main.c++
+++ main.c++
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World! youkoso watashi wa DSA society" << endl;
     return 0;
 }
--- practise/using2LoopMax.cpp
+++ practise/using2LoopMax.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <array>
+
  using namespace std;
 int main() {
-   int arr[]={1,2,3,4};
+    int max=0;
+   int arr[]={1,-2,3,6};
    int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size;i++){
+        int psun=0;
         for (int j =i;j<size;j++){
-            for(int k=i;k<=j;k++){
-                cout<<arr[k]<<" ";
+            psun+=arr[j];
+            if (psun>max){
+                max=psun;
             }
-            cout<<endl;
         }
     }
+    cout<<max;
 
     return 0;
 }
